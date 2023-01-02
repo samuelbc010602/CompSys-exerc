f64873c071dab45007fe005bd54b396e44561245
samuelbc010602 <90405397+samuelbc010602@users.noreply.github.com>
2023-01-02 09:51:21 +0100

    Add files via upload

---
+++ 220926_caching/3dopt.c
@@ -0,0 +1,30 @@
+/* You might want to use different values for L,M,N when performing benchmarks. */
+#define L 10
+#define M 10
+#define N 100
+
+long sum_array_3d(long a[L][M][N])
+{
+  long sum = 0;
+  int i, j, k;
+  for (i = 0; i < M; i++) {
+    for (j = 0; j < N; j++) {
+      for (k = 0; k < L; k++) {
+        sum += a[i][j][k];
+      }
+    }
+  }
+  return sum;
+}
+
+int main(){
+    int arr[100][5][3];
+    for(int first = 0;first<L;first++){
+        for(int second = 0;second < M;second++){
+            for(int third = 0;third<N;third++){
+                arr[first][second][third] = (first+second)-third;
+        }
+    }
+}
+printf("%lu \n",sum_array_3d(arr));
+}
